d0dc6d8a38b7a5f39aca3325c274d1a050201953
ganesh <ganesh@local>
2021-12-23 20:00:28 +0000

    Added filter for js files

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 //Sample JS File
+const API_KEY = "{SECURE_API_KEY}" //dummy  API key, just something random
+const makeAPICall = functioin(key) {
+	console.log("Making API Call ...")
+}
+makeAPICall(API_KEY);
